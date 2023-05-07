5369be874142766be9df5db1ba801d45a2129e91
ciklamini <115336048+ciklamini@users.noreply.github.com>
2023-05-07 19:56:05 +0200

    Update gfdatapi.py

--- gfdatapi.py
+++ gfdatapi.py
@@ -1,4 +1,6 @@
 '''
+meshstate s kombinaci 
+points 
 
 '''
 import pandas as pd
@@ -14,48 +16,49 @@ from dash_vtk.utils import to_mesh_state
 geo_struct = 'fibonacci'
 # geo_struct = 'plane'
 
-if      geo_struct == 'Beam2D':     vtk_type = 9
-elif    geo_struct == 'Beam3D':     vtk_type = 12
-elif    geo_struct == 'fibonacci':  vtk_type = 12
-elif    geo_struct == 'plane':      vtk_type = 10
+geo_struct_b2 = 'Beam2D.vtu'
+geo_struct_b3 = 'Beam3D.vtu'
+geo_struct_fs = 'fibonacci.vtu'
+geo_struct_pl = 'plane.vtu'
 
-vtu_file = geo_struct + '.vtu'
-    
-vtk_file_path = vtu_file # without states
-
-reader = vtk.vtkXMLUnstructuredGridReader()
-reader.SetFileName(vtk_file_path)
-reader.Update() 
-mesh_state =to_mesh_state(reader.GetOutput(), field_to_keep= 'Logarithmic_strain_Min_Principal')
-rng = [0,1]
-cs_name = 'Greens'
 app = dash.Dash(__name__)
 server = app.server
-vtk_view = dash_vtk.View(
-    [
-         dash_vtk.GeometryRepresentation(
+def defVTKfile(VTU_FILE:str):
+    vtk_file_path = VTU_FILE # without states
+
+    reader = vtk.vtkXMLUnstructuredGridReader()
+    reader.SetFileName(vtk_file_path)
+    reader.Update() 
+    mesh_state =to_mesh_state(reader.GetOutput(), field_to_keep= 'Logarithmic_strain_Min_Principal')
+    rng = [0,1]
+    cs_name = 'Greens'
+    
+    vtk_view = dash_vtk.View(
+        [dash_vtk.GeometryRepresentation(
             [
-                dash_vtk.Mesh(state = mesh_state),       
-                dash_vtk.PointCloudRepresentation(
-                    # xyz=points_01,
-                    # scalars=point_values,
-                    # colorDataRange=[min_elevation, max_elevation],
-                    property={"pointSize": 7, 
-                              "symbol": 'circle', }, ),                        
-             ],
-
-            property={"edgeVisibility": True, "opacity": .9872},
-            # colorMapPreset=cs_name,
-            colorDataRange=rng
-        )
-    ],
-    background=[1, 1, 1],
-)
-
+                    dash_vtk.Mesh(state = mesh_state),       
+                    dash_vtk.PointCloudRepresentation(
+                        property={"pointSize": 7, 
+                                  "symbol": 'circle', }, ),                        
+                 ],
+    
+                property={"edgeVisibility": True, "opacity": .9872},
+                colorDataRange=rng
+            )
+        ],
+        background=[1, 1, 1],
+    )
+    return vtk_view
+vtk_view_b2 = defVTKfile(geo_struct_b2)
+vtk_view_b3 = defVTKfile(geo_struct_b3)
+vtk_view_fs = defVTKfile(geo_struct_fs)
+vtk_view_pl = defVTKfile(geo_struct_pl)
 app.layout = html.Div(
     style={"height": "calc(100vh - 16px)"},
-    children=[html.Div(vtk_view,
-                       style={"height": "100%", "width": "100%", })],
+    children=[html.Div(vtk_view_b2, style={"height": "100%", "width": "100%", }),
+              html.Div(vtk_view_b3, style={"height": "100%", "width": "100%", }),
+              html.Div(vtk_view_fs, style={"height": "100%", "width": "100%", }),
+              html.Div(vtk_view_pl, style={"height": "100%", "width": "100%", })],
     
 )
 if __name__ == "__main__":    app.run_server(debug=True)
